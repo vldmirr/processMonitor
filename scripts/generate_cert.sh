3924c91b8c379599d3a5136ddc4fade9fc956ebf
vldmirr <tsutsura18@gmail.com>
2025-10-30 01:52:23 +0300

    create generate_cert.sh

---
+++ scripts/generate_cert.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir -p nginx/ssl
+
+# Генерируем SSL сертификаты
+openssl req -x509 -newkey rsa:4096 -nodes \
+    -out nginx/ssl/server.crt \
+    -keyout nginx/ssl/server.key \
+    -days 365 \
+    -subj "/CN=test.com" \
+    -addext "subjectAltName=DNS:test.com,DNS:localhost,IP:127.0.0.1"
+
+echo "SSL certificates generated in nginx/ssl/"
+echo "You can now run: docker-compose up -d"
