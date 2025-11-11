c714bf34fa945b37cbfa0b98c57cb6f964c668b1
vldmirr <tsutsura18@gmail.com>
2025-11-11 13:29:51 +0300

    Revert "refact" reset commit
    This reverts commit 70c10299ccdf77a4fe278a777423604746e8f289.

--- src/process_monitor.sh
+++ src/process_monitor.sh
@@ -59,7 +59,7 @@ checkProcess() {
         fi
     fi
 
-    #записываем прошлый PID
+    # читаем прошлый PID
     if [ -f "$PID_FILE" ]; then
         prevPID=$(cat "$PID_FILE" 2>/dev/null)
     fi
@@ -68,6 +68,9 @@ checkProcess() {
     if [ -n "$prevPID" ] && [ "$currPID" != "$prevPID" ]; then  
         log "INFO: Process $NAME was restarted. Old PID: $prevPID, New PID: $currPID"
     fi
+
+    # Сохраняем текущий PID
+    echo "$currPID" > "$PID_FILE"
     
     # Отправляем запрос на сервер
     if sendReq; then
